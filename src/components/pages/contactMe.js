39a7a61d9cd5e4689aa8383d1c4f4934dd726ffd
Jayden Chapman <jaychapmancode@gmail.com>
2023-02-28 14:40:30 -0800

    Added resume

--- src/components/pages/contactMe.js
+++ src/components/pages/contactMe.js
@@ -1,7 +1,26 @@
 import React, { useState } from 'react';
 
 export default function ContactMe() {
-    <h1>How to Contact Me:</h1>
+    return (
+        <div>
+        <h1 className="text-center">Contact Me</h1>
+        <form>
+            <div className="form-group">
+                <label>Email address</label>
+                <input type="email" className="form-control" aria-describedby="emailHelp" />
+            </div>
+            <div className="form-group">
+                <label>Name</label>
+                <input className="form-control" id="exampleInputPassword1" />
+            </div>
+            <div className="form-group">
+            <label>Message:</label>
+            <textarea className="form-control"rows="2"></textarea>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+        </div>
+    )
 };
 
 //input="email" or type="email" required 
--- src/components/pages/resume.js
+++ src/components/pages/resume.js
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
 
 export default function Resume() {
-
+    return (
+        <div>
+            <h1>Résumé</h1>
+            <img src='./images/resume.png' />
+        </div>
+    )
 };
