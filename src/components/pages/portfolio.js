e530767432ad6c6b6d8a87872ced0e815187c34c
Jayden Chapman <jaychapmancode@gmail.com>
2023-02-28 14:27:11 -0800

    moved images folder to public

--- src/components/pages/portfolio.js
+++ src/components/pages/portfolio.js
@@ -3,13 +3,37 @@ import React, { useState } from 'react';
 export default function Portfolio() {
     const projects = [
         {
+            id: 0,
             name: "Movie Recommender",
             image: "./images/MovieRec.png",
-            description: "A project where I worked with 3 other developers to create a small movie recommendation application for those struggling with something to watch"
+            description: "A project where I worked with 3 other developers to create a small movie recommendation application for those struggling with something to watch",
+            link: "https://hayni100.github.io/Movie-Recs/"
         },
         {
+            id: 1,
             name: "newDND",
-            image: ""
-        }
+            image: "./images/newDND.png",
+            description: "A turn-based web game that takes a logged-in user through a small dungeons using turn based logic, RNG, and allows for small fighting exchanges with monsters encountered in the dungeon",
+            link: "https://new-dnd7.herokuapp.com/"
+        },
     ];
+
+    return (
+        <div>
+            <h1 className="text-center">Projects</h1>
+            <ul className="pl-0">
+                {projects.map(project => (
+                    <div className="card" key={project.id}>
+                    <img src={project.image} className="card-img-top" />
+                        <div className="card-body">
+                            <h5 className="card-title">{project.name}</h5>
+                            <p className="card-text">{project.description}</p>
+                            <a href={project.link} className="btn btn-primary">Visit</a>
+                        </div>
+                    </div>
+                ))}
+            </ul>
+        </div>
+    );
+
 };
