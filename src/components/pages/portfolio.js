de720f3eae1ace3232fffccf3a5c31ca539922e9
Jayden Chapman <jaychapmancode@gmail.com>
2023-02-28 14:01:39 -0800

    Created images folder, works on portfolio page

--- src/components/pages/portfolio.js
+++ src/components/pages/portfolio.js
@@ -2,6 +2,14 @@ import React, { useState } from 'react';
 
 export default function Portfolio() {
     const projects = [
-
+        {
+            name: "Movie Recommender",
+            image: "./images/MovieRec.png",
+            description: "A project where I worked with 3 other developers to create a small movie recommendation application for those struggling with something to watch"
+        },
+        {
+            name: "newDND",
+            image: ""
+        }
     ];
 };
